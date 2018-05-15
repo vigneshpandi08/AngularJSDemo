e3fde191ed3945f41c0c36896fd4bda4cad5a646
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-05-15 16:54:50 +0530

    Added project

---
+++ AngularJsDemo/AngularJsDemo/Scripts/MyApp.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('myApp'[]);
+
+app.controller('demoController', function ($scope) {
+    $scope.val = "Hai AngularJS";
+
+    $scope.myFun = function () {      
+            alert('Hai welcome');
+        }
+
+
+});
